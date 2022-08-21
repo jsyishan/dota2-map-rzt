5e4e2a6e2171e1c357764cc4becb61340d1cbbb0
jsyishan <404678173@qq.com>
2022-08-22 00:46:08 +0800

    feat(event): add global interval event

--- game/scripts/src/framework/system/event/event_system.ts
+++ game/scripts/src/framework/system/event/event_system.ts
@@ -1,6 +1,27 @@
 import ISystem from "../system-interface"
-import IEvent from "./event-interface";
+import IEvent from "./event-interface"
 
+type IntervalCallback = () => void
+
+const intervals: Array<IntervalEvent> = [] 
+class IntervalEvent {
+    public readonly callback: IntervalCallback
+    public readonly interval: number
+    public timer: number
+
+    constructor(callback: IntervalCallback, interval: number) {
+        this.callback = callback
+        this.interval = interval
+        this.timer = 0
+    }
+
+    public stop() {
+        const idx = intervals.indexOf(this)
+        if (idx > -1) {
+            intervals.splice(idx, 1)
+        }
+    }
+}
 
 export default class EventSystem implements ISystem {
     rawRegisterEvent(eventName: keyof GameEventDeclarations, callback: (event: GameEventProvidedProperties & GameEventDeclarations[keyof GameEventDeclarations]) => void) {
@@ -14,11 +35,23 @@ export default class EventSystem implements ISystem {
         }, this)
     }
 
+    registerInterval(callback: IntervalCallback, interval: number): IntervalEvent {
+        const event = new IntervalEvent(callback, interval)
+        intervals.push(new IntervalEvent(callback, interval))
+        return event
+    }
+
     onStart() {
 
     }
 
     onUpdate(dt: number) {
-
+        intervals.forEach(e => {
+            e.timer += dt
+            if (e.timer >= e.interval) {
+                e.callback()
+                e.timer = 0
+            }
+        })
     }
 }
