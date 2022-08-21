a5f998eb6051b33e82913f381d3b499f006f54d6
jsyishan <404678173@qq.com>
2022-08-21 23:37:24 +0800

    feat(log): add Log.show

--- game/scripts/src/framework/utils/logger.ts
+++ game/scripts/src/framework/utils/logger.ts
@@ -1,16 +1,17 @@
 enum LogLevel {
-    Info  = "I",
-    Warn  = "W",
-    Error = "E"
+    Info = "I",
+    Show = "S",
+    Warn = "W",
+    Error = "E",
 }
 
 function dump(o: any) {
-    if (typeof(o) === 'object') {
+    if (typeof (o) === 'object') {
         let s = '{ '
         Object.entries<object>(o).forEach(kv => {
             let k = kv[0]
             let v = kv[1]
-            if (typeof(k) !== 'number') {
+            if (typeof (k) !== 'number') {
                 k = `"${k}"`
             }
             s += `[${k}] = ${dump(v)}, `
@@ -25,7 +26,7 @@ function getInfo(...args) {
     let info
     if (args.length > 1) {
         info = dump(args)
-    } else if (args.length === 1 && typeof(args[0]) === 'object') {
+    } else if (args.length === 1 && typeof (args[0]) === 'object') {
         info = dump(args)
     } else {
         info = args[0]
@@ -37,15 +38,23 @@ function _print(tag, level, ...args) {
     print(`${GetSystemDate()}-${GetSystemTime()} [${tag}][${level}] ${getInfo(...args)}`)
 }
 export default class Log {
-    static i(tag, ...args) {
+    static i(tag: string, ...args) {
         _print(tag, LogLevel.Info, ...args)
     }
 
-    static w(tag, ...args) {
+    static w(tag: string, ...args) {
         _print(tag, LogLevel.Warn, ...args)
     }
 
-    static e(tag, ...args) {
+    static e(tag: string, ...args) {
         _print(tag, LogLevel.Error, ...args)
+    }
+
+    static show(tag: string, entity: CBaseEntity, all: boolean, msg: string) {
+        if (!entity) {
+            entity = GameRules.GetGameModeEntity()
+        }
+        _print(tag, LogLevel.Show, msg)
+        Say(entity, msg, !all)
     }
 }
